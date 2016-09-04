1f9a3a258397fb28ef777d3ca8749a249abde84b
Matthew Brown <barakis@barakisbrown.com>
2016-09-04 16:33:52 -0500

    Model for a single blood sugar reading

---
+++ MyApplication/app/src/main/java/com/lokislayer/bloodsugartracker/Model/BloogSugarModel.java
@@ -0,0 +1,44 @@
+package com.lokislayer.bloodsugartracker.Model;
+
+/**
+ * Created by Matthew on 9/4/2016.
+ * The underlying model for the application which stores all information about a blood sugar
+ * reading
+ */
+public class BloogSugarModel {
+    public static final String ID = "id";
+    public static final String DATE_TESTED = "date_tested";
+    public static final String TIME_TESTED = "time_tested";
+    public static final String AMOUNT = "amount";
+
+    private String _dateTested;
+    private String _timeTested;
+    private int _amount;
+
+    public String getDateTested() {
+        return _dateTested;
+    }
+
+    public String getTimeTested() {
+        return _timeTested;
+    }
+
+    public int getAmount() {
+        return _amount;
+    }
+
+    public void setDateTested(String dateTested)
+    {
+        _dateTested = dateTested;
+    }
+
+    public void setTimeTested(String timeTested)
+    {
+        _timeTested = timeTested;
+    }
+
+    public void setAmount(int amount)
+    {
+        _amount = amount;
+    }
+}
