525b50b25e47d9c1f39d523f3c80c769c1848b5c
Matthew Brown <barakis@barakisbrown.com>
2016-09-04 17:22:49 -0500

    Main Activity done except for the settings part

--- MyApplication/app/src/main/java/com/lokislayer/bloodsugartracker/MainActivity.java
+++ MyApplication/app/src/main/java/com/lokislayer/bloodsugartracker/MainActivity.java
@@ -1,16 +1,44 @@
 package com.lokislayer.bloodsugartracker;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.lokislayer.bloodsugartracker.DB.DatabaseHelper;
+import com.lokislayer.bloodsugartracker.Fragments.DatePickerFragment;
+import com.lokislayer.bloodsugartracker.Fragments.TimePickerFragment;
+import com.lokislayer.bloodsugartracker.Model.BloodSugarModel;
+
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener,DatePickerFragment.DataReturnedListener, TimePickerFragment.TimeEnteredListener
+{
+    private String dateStr,timeStr;
+    private TextView minText;
+    private TextView maxText;
+    private TextView avgText;
+    private EditText amountTested;
+    private Button dateTested;
+    private Button timeTested;
+    private Button submitResults;
+    private DatabaseHelper db;
+    private BloodSugarModel model;
+    private int amountBlood;
+
+    // NEED THESE TWO FRAGMENTS SO THAT I CAN ACCESS THEM THROUGHOUT THE APP
+    DatePickerFragment dateFragment;
+    TimePickerFragment timeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +47,25 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // INITIALIZE DB FIELD
+        db = new DatabaseHelper(this);
+        LoadOrUpdateValues();
+
+        // BUTTONS THAT NEED BE INITIATED AND BOUND TO THE CLICK LISTENER
+        dateTested = (Button)findViewById(R.id.dateEnteredBtn);
+        dateTested.setOnClickListener(this);
+
+        timeTested = (Button)findViewById(R.id.timeEnteredBtn);
+        timeTested.setOnClickListener(this);
+
+        submitResults = (Button)findViewById(R.id.submitResult);
+        submitResults.setOnClickListener(this);
+
+        // INITIALIZE FRAGMENTS
+        dateFragment = new DatePickerFragment();
+        timeFragment = new TimePickerFragment();
+
+        // MIGHT REMOVE
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,8 +74,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        //
 
-        // THIS IS A TEST LINE
     }
 
     @Override
@@ -54,5 +101,112 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onGetDataEntered(String str)
+    {
+        dateStr = str;
+        // DEBUG
+        Log.d("Date[Main Activity] =",dateStr);
+        //
+        dateTested = (Button)findViewById(R.id.dateEnteredBtn);
+        dateTested.setText(str);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        minText = (TextView)findViewById(R.id.minValue);
+        maxText = (TextView)findViewById(R.id.maxValue);
+        avgText = (TextView)findViewById(R.id.avgValue);
+
+        if (v.getId() == R.id.submitResult)
+        {
+            // VALIDATE FIRST
+            if (validation()) {
+                amountTested = (EditText) findViewById(R.id.bloodAmount);
+                amountBlood = Integer.parseInt(amountTested.getText().toString());
+                Log.d("Submit Results", "");
+                Log.d("Blood AMount(int)", String.valueOf(amountBlood));
+                Log.d("Date Tested : ", dateStr);
+                Log.d("Time Tested : ", timeStr);
+                InsertTestResults();
+
+                // UPDATE THE UI SINCE INSERT HAS BEEN DONE
+                dateTested = (Button) findViewById(R.id.dateEnteredBtn);
+                timeTested = (Button) findViewById(R.id.timeEnteredBtn);
+                amountTested.setText("");
+                dateTested.setText(R.string.date_btn_text);
+                timeTested.setText(R.string.time_btn_text);
+                dateFragment.setDefault(true);
+            }
+        }
+        else if (v.getId() == R.id.dateEnteredBtn) {
+            dateFragment.show(getSupportFragmentManager(), "datePicker");
+        }
+        else if (v.getId() == R.id.timeEnteredBtn)
+        {
+            timeFragment.show(getSupportFragmentManager(),"timePicker");
+        }
+    }
+
+    @Override
+    public void onTimeEntered(String str)
+    {
+        timeStr = str;
+        // DEBUG
+        Log.d("Time[Main Activity] =",timeStr);
+        //
+        timeTested = (Button)findViewById(R.id.timeEnteredBtn);
+        timeTested.setText(str);
+    }
+
+    private void InsertTestResults()
+    {
+        model = new BloodSugarModel();
+        model.setAmount(amountBlood);
+        model.setDateTested(dateStr);
+        model.setTimeTested(timeStr);
+        db.addTestResults(model);
+        LoadOrUpdateValues();
+    }
+
+    private void LoadOrUpdateValues()
+    {
+        // NOTE: Will be set to the actual values when the app launches
+        minText = (TextView)findViewById(R.id.minValue);
+        maxText = (TextView)findViewById(R.id.maxValue);
+        avgText = (TextView)findViewById(R.id.avgValue);
+        // display values
+        minText.setText(String.valueOf(db.getMinAmount()));
+        avgText.setText(String.valueOf(db.getAvgAmount()));
+        maxText.setText(String.valueOf(db.getMaxAmount()));
+    }
+
+    public boolean validation()
+    {
+        StringBuilder build = new StringBuilder();
+        amountTested = (EditText)findViewById(R.id.bloodAmount);
+
+        if (amountTested.getText().toString().equals("")){
+            build.append("Amount Tested is REQUIRED \n");
+        }
+        if (dateStr == null) {
+            build.append("DATE IS REQUIRED \n");
+        }
+        if (timeStr == null){
+            build.append("TIME IS REQUIRED \n");
+        }
+
+        if (!build.toString().isEmpty())
+        {
+            Context ctx = getApplicationContext();
+            String errMsg = build.toString();
+            int duration = Toast.LENGTH_LONG;
+            Toast.makeText(ctx, errMsg, duration).show();
+            return false;
+        }
+        return true;
     }
 }
