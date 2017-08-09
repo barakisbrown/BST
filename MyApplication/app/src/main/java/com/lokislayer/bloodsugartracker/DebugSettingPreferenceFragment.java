1a8204612b6359c2f37abe57dca0d642cc87adec
Matthew Brown <lokislayer@gmail.com>
2017-08-09 13:18:05 -0500

    Added Debug Preference

---
+++ MyApplication/app/src/main/java/com/lokislayer/bloodsugartracker/DebugSettingPreferenceFragment.java
@@ -0,0 +1,10 @@
+package com.lokislayer.bloodsugartracker;
+
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by barak on 7/30/2017.
+ */
+
+public class DebugSettingPreferenceFragment extends PreferenceFragment {
+}
---
+++ MyApplication/app/src/main/java/com/lokislayer/bloodsugartracker/DebugSettingsActivity.java
@@ -0,0 +1,10 @@
+package com.lokislayer.bloodsugartracker;
+
+import android.preference.PreferenceActivity;
+
+/**
+ * Created by barak on 7/30/2017.
+ */
+
+public class DebugSettingsActivity extends PreferenceActivity {
+}
